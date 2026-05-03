c07d7eb1230864ffe8000d82f3eac8db53734cc1
wldrwidya <widya.535210100@stu.untar.ac.id>
2026-05-03 21:41:30 +0700

    migrate sqlite to supabase

--- main.py
+++ main.py
@@ -8,29 +8,37 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 from pydantic import BaseModel, Field
-import sqlite3
+import psycopg2
+import os
 from datetime import datetime
 from pathlib import Path
 
 BASE_DIR = Path(__file__).parent
-DB_PATH = BASE_DIR / "reviews.db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 # ========================
 # DATABASE
 # ========================
 def get_db():
-    conn = sqlite3.connect(str(DB_PATH))
-    conn.execute("""
+    conn = psycopg2.connect(DATABASE_URL)
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
         CREATE TABLE IF NOT EXISTS reviews (
-            review_id INTEGER PRIMARY KEY AUTOINCREMENT,
+            review_id SERIAL PRIMARY KEY,
             menu_name TEXT NOT NULL,
-            rating INTEGER NOT NULL CHECK(rating BETWEEN 1 AND 5),
+            rating INTEGER CHECK(rating BETWEEN 1 AND 5),
             review_text TEXT,
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
     """)
+
     conn.commit()
+    cursor.close()
+
     return conn
+
 
 # init DB
 get_db().close()
@@ -56,6 +64,7 @@ class ReviewIn(BaseModel):
     rating: int = Field(ge=1, le=5)
     review_text: str = ""
 
+
 # ========================
 # API ROUTES
 # ========================
@@ -63,23 +72,38 @@ class ReviewIn(BaseModel):
 @app.post("/api/reviews")
 def create_review(review: ReviewIn):
     conn = get_db()
-    conn.execute(
-        "INSERT INTO reviews (menu_name, rating, review_text) VALUES (?, ?, ?)",
+    cursor = conn.cursor()
+
+    cursor.execute(
+        "INSERT INTO reviews (menu_name, rating, review_text) VALUES (%s, %s, %s)",
         (review.menu_name, review.rating, review.review_text),
     )
+
     conn.commit()
+
+    cursor.close()
     conn.close()
-    return {"status": "success", "message": "Review berhasil disimpan!"}
+
+    return {
+        "status": "success",
+        "message": "Review berhasil disimpan!"
+    }
 
 
 @app.get("/api/reviews")
 def list_reviews():
     conn = get_db()
-    rows = conn.execute("""
+    cursor = conn.cursor()
+
+    cursor.execute("""
         SELECT review_id, menu_name, rating, review_text, created_at
         FROM reviews
         ORDER BY created_at DESC
-    """).fetchall()
+    """)
+
+    rows = cursor.fetchall()
+
+    cursor.close()
     conn.close()
 
     return [
@@ -93,11 +117,13 @@ def list_reviews():
         for r in rows
     ]
 
+
 # ========================
 # STATIC (FRONTEND)
 # ========================
 
 app.mount("/static", StaticFiles(directory=BASE_DIR), name="static")
+
 
 @app.get("/")
 def serve_index():
